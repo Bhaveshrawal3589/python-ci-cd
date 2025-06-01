81823359e45a970c1c8c6ed23abdfe4d55bcee4f
Bhavesh Rawal <bhaveshrawal9997@gmail.com>
2025-06-01 20:09:47 +0530

    Added logging to Flask routes

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
-
+import logging
 from flask import Flask
+
+# Configure logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
+    logging.info("Root endpoint '/' was accessed")
     return 'Hello from Flask on Render!'
 
 @app.route('/new')
 def new_route():
+    logging.info("New endpoint '/new' was accessed")
     return 'This is a new route for auto-deployment test!'
+
 
 
