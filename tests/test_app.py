d7029bef605321889d22b66416e18220bc9b4ed0
Bhavesh Rawal <bhaveshrawal9997@gmail.com>
2025-06-01 12:45:28 +0530

    Add app and test code, plus init files for test discovery

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,10 @@
-"from app import add" 
-"" 
-"def test_add():" 
-"    assert add(2, 3) == 5" 
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from app import add
+
+def test_add():
+    assert add(2, 3) == 5
+
