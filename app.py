36be196d13054831e5092436b4dc1432a3155c88
Bhavesh Rawal <bhaveshrawal9997@gmail.com>
2025-06-01 17:21:54 +0530

    Add /new route, test cases, and CI config

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
+
 from flask import Flask
 
-app = Flask(__name__)  # <--- this line defines the `app` that gunicorn looks for
+app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return 'Hello from Flask on Render!'
 
-
+@app.route('/new')
+def new_route():
+    return 'This is a new route for auto-deployment test!'
 
 
