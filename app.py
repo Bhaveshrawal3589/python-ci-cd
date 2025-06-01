8ebc00225a646aab0c54909d617cc0efe01fe2fb
Bhavesh Rawal <bhaveshrawal9997@gmail.com>
2025-06-01 14:01:44 +0530

    Fix: define app object for gunicorn

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
-def add(a, b):
-    return a + b
+from flask import Flask
+
+app = Flask(__name__)  # <== This line defines the `app` object
+
+@app.route('/')
+def home():
+    return 'Hello from Render!'
+
 
